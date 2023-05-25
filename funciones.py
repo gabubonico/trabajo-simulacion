5e1f0e6bee3c361ff0ba1cc8ddde5424d321e51a
Gabriel Cortés Rodríguez <gabrielcortes@uma.es>
2023-05-25 13:56:05 +0200

    refactor

--- funciones.py
+++ funciones.py
@@ -1,21 +1,9 @@
 import random
 
-def fac(n):
-    if n == 0:
+def ber(p):
+    if random.random() <= p:
         return 1
     else:
-        return n * fac(n - 1)
+        return 0
 
-def compose(n, x):
-    return fac(n)/(fac(x) * fac(n - x))
 
-def probabilidad_binomial(x, n, p):
-    return compose(n, x) * pow(p, x) * pow(1-p,x-n)
-
-def distribucion(n, p):
-    resul = []
-
-    for i in range(n):
-        resul.append(probabilidad_binomial(i, n, p))
-
-    return resul
