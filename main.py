070a7c68932e939ee0ae43879cb76fb11de33fa8
Gabriel Cortés Rodríguez <gabrielcortes@uma.es>
2023-05-27 16:18:10 +0200

    media y varianza

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import sys
+import statistics
 
+# variables de bernoulli
 def ber(p):
     if random.random() <= p:
         # pasa la prueba
@@ -9,6 +11,7 @@ def ber(p):
         # no pasa la prueba
         return False
 
+# variables binomiales
 def bin(p):
     # variable que recoge el numero de exitos
     exitos = 0
@@ -21,6 +24,7 @@ def bin(p):
     # devolvemos el numero de exitos
     return exitos
 
+# muestra de valores binomiales
 def valores_binomial(n, p):
     # almacenaremos los resultados en un array
     resultado = []
@@ -35,6 +39,16 @@ def valores_binomial(n, p):
     # devolvemos el array de valores
     return resultado
 
+# calcular la media
+def media(X):
+    suma = sum(X)
+    media = suma / len(X)
+    return media
+
+# calcular la varianza
+def varianza(X):
+    var = statistics.variance(X)
+    return var
 
 def main():
     input("Bienvenido a mi trabajo de simulacion (presione ENTER para continuar)")
@@ -55,7 +69,7 @@ def main():
 
     print("la muestra ha sido generada")
 
-    # muestra los valores por terminal
+    # muestra los valores por terminal si recibe el argumento --verbose
     if len(sys.argv) > 1 and sys.argv[1] == "--verbose":
         print("los valores son estos: ")
         print(valores)
@@ -71,6 +85,17 @@ def main():
             file.write(f"{value} {frequency}\n")
 
     print("los datos de la distribucion binomial han sido guardados en el fichero binomial.txt")
-    input("usa 'gnuplot binomial.txt' para generar el historiograma (ENTER )")
+    input("usa 'gnuplot binomial.p' para generar el historiograma (ENTER para continuar)")
+
+    # estimacion puntual de la media y la varianza
+    m = media(valores)
+    v = varianza(valores)
+
+    print("la media de esta muestra es: ")
+    print(m)
+    print("la varianza de esta muestra es: ")
+    print(v)
+    input("presione ENTER para continuar")
+
 if __name__ == "__main__":
     main()
